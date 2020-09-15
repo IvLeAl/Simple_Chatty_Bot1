a089dd524d66feb09f3a21ea508e1eb0e565464e
Владелец <leonid82@yandex.ru>
2020-09-15 10:56:27 +0300

    Решение задач

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String yourName = scanner.next();
 
         System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2020.");
+        System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
-        System.out.println("What a great name you have, " + yourName + "!");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+
+        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
     }
 }
