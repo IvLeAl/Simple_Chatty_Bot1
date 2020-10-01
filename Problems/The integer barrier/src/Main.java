0d95d14f4d1bde34612c6e9ab7e23e5b2a85b2d1
Владелец <leonid82@yandex.ru>
2020-10-01 11:43:11 +0300

    новые задачи

--- Problems/The integer barrier/src/Main.java
+++ Problems/The integer barrier/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            int a = input.nextInt();
+            sum = sum + a;
+            if (sum >= 1000) {
+                sum -= 1000;
+                break;
+            }
+            if (a == 0)
+                break;
+
+        }
+        System.out.println(sum);
     }
 }
