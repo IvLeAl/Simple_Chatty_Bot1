27322b7ed732e104c255bc9699c982847193d0b7
Владелец <leonid82@yandex.ru>
2020-10-02 13:44:41 +0300

    Добавлены  задачи

--- Problems/The (un)-ordered sequence/src/Main.java
+++ Problems/The (un)-ordered sequence/src/Main.java
@@ -4,30 +4,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 0;
+        for ( int i = 0; i <= a ; i++) {
+            for ( int j = 0; j < i ; j++) {
+                if ( b == a) break;
+                b++;
+                System.out.println(i);
 
-
-        while(scanner.hasNext()){
-            int num1 = scanner.nextInt();
-            int num2 = scanner.nextInt();
-            int num3 = scanner.nextInt();
-            if (num1 == 0 || num2 == 0 || num3 == 0){
-                break;
-            }
-
-            if(num1 > num2 && num2 > num3){
-                System.out.print(true);
-
-            }
-
-            else if (num3 > num2 && num3 > num1 && num2 > num1){
-                System.out.print(true);
-
-            }
-
-            else {
-                System.out.print(false);
             }
         }
-
     }
 }
---
+++ Problems/The sequence (1)/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 0;
+        for ( int i = 0; i <= a ; i++) {
+            for ( int j = 0; j < i ; j++) {
+                if ( b == a) break;
+                b++;
+                System.out.println(i);
+
+            }
+        }
+    }
+}
