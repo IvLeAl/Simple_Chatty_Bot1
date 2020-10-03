452bf91aa7fb167f039e202d765ec91e7f452a0f
Владелец <leonid82@yandex.ru>
2020-10-03 22:55:41 +0300

    Задачи

--- Problems/The sequence (1)/src/Main.java
+++ Problems/The sequence (1)/src/Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = 0;
-        for ( int i = 0; i <= a ; i++) {
-            for ( int j = 0; j < i ; j++) {
-                if ( b == a) break;
-                b++;
-                System.out.println(i);
-
+        int oldNum = scanner.nextInt();
+        int newNum;
+        int asc = 0;
+        int dec = 0;
+        while ((newNum = scanner.nextInt()) != 0) {
+            if (newNum > oldNum) {
+                asc++;
             }
+            if (newNum < oldNum) {
+                dec++;
+            }
+            oldNum = newNum;
         }
+        System.out.println(asc == 0 || dec == 0);
     }
 }
