4221745ff2a28fb76763b6f03f79abe58d392e3e
Владелец <leonid82@yandex.ru>
2020-10-05 18:03:34 +0300

    Добавлены задачи

--- Problems/Raise to the power/src/Main.java
+++ Problems/Raise to the power/src/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static long power(int n, int m) {
         // write your code here
+        int d = 0;
+        for (int i = 0; i <= m; i++) {
+            d *= n;
+                return 1;
+
+        }
     }
+
+
+
 
     /* Do not change code below */
     public static void main(String[] args) {
