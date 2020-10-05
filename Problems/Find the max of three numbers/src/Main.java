82d6a07bcf80c9eaf9ddc3e60267ae22a2446852
Владелец <leonid82@yandex.ru>
2020-10-05 15:40:43 +0300

    Добавлены задачи

--- Problems/Find the max of three numbers/src/Main.java
+++ Problems/Find the max of three numbers/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static int getNumberOfMaxParam(int a, int b, int c) {
         // write a body here
+        if (a >= b && a >= c) {
+            return  1;
+        }else if (b >= a && b >= c) {
+            return  2;
+        }else return 3;
+
     }
 
     public static void main(String[] args) {
